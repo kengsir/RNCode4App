139cf3088c66195ce1a712ccedb4bc366b1759ae
LeeJay <kengsir@KengsirdeMacBook-Pro.local>
2016-10-16 19:14:17 +0800

    first commit

---
+++ Code4App/Code4App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Code4App
+//
+//  Created by Kengsir on 2016/10/16.
+//  Copyright © 2016年 com.imessage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code4App/Code4App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Code4App
+//
+//  Created by Kengsir on 2016/10/16.
+//  Copyright © 2016年 com.imessage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
